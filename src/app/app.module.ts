f162a9ca611bb9a38554866885db325a9f48464f
ktsrivastava29 <ktsrivastava29@gmail.com>
2025-02-03 23:17:53 +0530

    minor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { AddWorkoutComponent } from './components/add-workout/add-workout.component';
 import { WorkoutListComponent } from './components/workout-list/workout-list.component';
+import { WorkoutChartComponent } from './components/workout-chart/workout-chart.component';
 import { WorkoutService } from './services/workout.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddWorkoutComponent,
-    WorkoutListComponent
+    WorkoutListComponent,
+    WorkoutChartComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    CommonModule, 
   ],
   providers: [WorkoutService],
   bootstrap: [AppComponent],
-  exports:[AddWorkoutComponent]
 })
 export class AppModule { }
